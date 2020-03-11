67b2dc88cfa82ec97895f16477bee0016584d5ee
hhb584520 <haibin.huang@intel.com>
2020-03-11 14:05:17 +0800

    Create curl_flavor.sh

---
+++ onap/debug/curl_flavor.sh
@@ -0,0 +1,9 @@
+curl -k -X GET \
+  'https://172.30.3.105:30233/aai/v14/cloud-infrastructure/cloud-regions/cloud-region/CloudOwner/ONAP-Pod-01-Rail-07/flavors/?depth=all' \
+  -H 'Accept: application/json' \
+  -H 'Authorization: Basic QUFJOkFBSQ==' \
+  -H 'Postman-Token: b8a403f0-9c07-4242-b52b-20782db2959a' \
+  -H 'Real-Time: true' \
+  -H 'X-FromAppId: jimmy-postman' \
+  -H 'X-TransactionId: 9999' \
+  -H 'cache-control: no-cache' 
