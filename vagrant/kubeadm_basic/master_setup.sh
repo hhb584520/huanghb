eb1d67da2e89ad70ace7c5df9b739721014e0aaa
Haibin Huang <haibin.huang@intel.com>
2020-12-04 12:41:42 +0800

    Add flannel and libvirt vm
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- vagrant/kubeadm_basic/master_setup.sh
+++ vagrant/kubeadm_basic/master_setup.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-sudo kubeadm init --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
+sudo kubeadm init --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.244.0.0/16 --token 8c5adc.1cec8dbf339093f0
 mkdir ~/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f http://git.io/weave-kube-1.6
+kubectl apply -f /vagrant/kube-flannel.yml
