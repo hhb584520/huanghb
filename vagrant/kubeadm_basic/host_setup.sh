bcf355e534413c85d32a48a37ec69537319664f9
Haibin Huang <haibin.huang@intel.com>
2021-04-28 11:37:20 +0800

    Update host_setup.sh

--- vagrant/kubeadm_basic/host_setup.sh
+++ vagrant/kubeadm_basic/host_setup.sh
@@ -35,7 +35,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated kubelet=1.15.2-00 kubeadm=1.15.2-00 kubectl=1.15.2-00 kubernetes-cni=0.7.5-00
+sudo apt-get install -y --allow-unauthenticated kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00 kubernetes-cni=0.8.7-00
 sudo sed -i '9i\Environment="KUBELET_EXTRA_ARGS=--feature-gates=DevicePlugins=true"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 sudo modprobe ip_vs
