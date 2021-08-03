5dc5d7dd816441aa975794a2654250895ffc3103
Haibin Huang <haibin.huang@intel.com>
2021-08-03 13:58:08 +0800

    Update host_setup.sh

--- vagrant/kubeadm_basic/baremetel/host_setup.sh
+++ vagrant/kubeadm_basic/baremetel/host_setup.sh
@@ -34,6 +34,9 @@ curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add 
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
+sh ./configure_docker_proxy.sh
+
 sudo apt-get update
 sudo apt-get install -y --allow-unauthenticated kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 kubernetes-cni=0.8.7-00
 
