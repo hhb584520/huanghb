6b0173209e016b28102c121fb5acb1fd9c53c0ea
Haibin Huang <haibin.huang@intel.com>
2021-08-04 14:36:25 +0800

    delete ansible
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

---
+++ linux_tools/setup_env/06_ssh_nopasswd.sh
@@ -0,0 +1,7 @@
+# A send command to A and B, A and B excute it.
+# A huc
+# B cloud-client5
+sudo apt-get install sshpass
+ssh-keygen
+ssh-copy-id -i ~/.ssh/id_rsa.pub haibin@hnuc
+ssh-copy-id -i ~/.ssh/id_rsa.pub haibin@cloud-client5
